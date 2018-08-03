5d2d099296e230e6a7369fa7932d5ae0b16ff8cb
yechen <yechen@acm.org>
2018-08-02 18:47:05 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 
+"""
+This is program is used to test distribute python project
+with pipenv and requests
+
+"""
 response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
