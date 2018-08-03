f2e22d5d41438a9cb50d24ef09c063c6362bc13d
yechen <yechen@acm.org>
2018-08-03 14:25:25 -0700

    trend analysis

--- history_price.py
+++ history_price.py
@@ -9,6 +9,9 @@ class dp_trend(object):
 	def __init__(self, dp):
 		super(dp_trend, self).__init__()
 		self.dp = dp
+		self.up_trend = []
+		self.down_trend = []
+
 
 	def min_dp(self):
 		return min(self.dp)
@@ -16,7 +19,109 @@ class dp_trend(object):
 	def max_dp(self):
 		return max(self.dp)
 
+	def up_down_trend(self):
+		print("trend")
+		sim = self.dp
+		print(sim)
+		print(len(sim))
 
+		up = []
+		down = []
+		trend_mark =""
+		#information to record
+		item = [0]
+		for i in range(1, len(sim)):
+			if trend_mark == "":
+				if sim[i] > sim[i-1]:
+					trend_mark = "up"
+					# days in the trend
+					item[0] = item[0] +1 
+				if sim[i] < sim[i-1]:
+					trend_mark = "down"
+					# days in the trend
+					item[0] = item[0] +1 
+			if trend_mark == "up":
+				#include equal in the up trend
+				if sim[i] >= sim[i-1]:
+					trend_mark = "up"
+					# days in the trend
+					item[0] = item[0] +1 
+				if sim[i] < sim[i-1]:
+					#trend turning point
+					trend_mark = ""
+					up.append(item)
+					item = [0]
+			if trend_mark == "down":
+				if sim[i] > sim[i-1]:
+					#trend turning point
+					trend_mark = ""
+					down.append(item)
+					item = [0]
+				if sim[i] <= sim[i-1]:
+					trend_mark = "down"
+					# days in the trend
+					item[0] = item[0] +1 
+		#process last group 
+		if trend_mark == "up":
+			up.append(item)
+		if trend_mark == "down":
+			down.append(item)
+		self.up_trend = up
+		self.down_trend = down
+
+	def print_trend(self):
+		"""visulization of the trend"""
+		self.up_down_trend()
+		print(self.up_trend)
+		print(self.down_trend)
+
+	def test_trend(self):
+		sim = [1,1,2,3,1,2,1,2,4,6,6,5,3,2,2]
+		print(len(sim))
+
+		up = []
+		down = []
+		trend_mark =""
+		#information to record
+		item = [0]
+		for i in range(1, len(sim)):
+			if trend_mark == "":
+				if sim[i] > sim[i-1]:
+					trend_mark = "up"
+					# days in the trend
+					item[0] = item[0] +1 
+				if sim[i] < sim[i-1]:
+					trend_mark = "down"
+					# days in the trend
+					item[0] = item[0] +1 
+			if trend_mark == "up":
+				#include equal in the up trend
+				if sim[i] >= sim[i-1]:
+					trend_mark = "up"
+					# days in the trend
+					item[0] = item[0] +1 
+				if sim[i] < sim[i-1]:
+					#trend turning point
+					trend_mark = ""
+					up.append(item)
+					item = [0]
+			if trend_mark == "down":
+				if sim[i] > sim[i-1]:
+					#trend turning point
+					trend_mark = ""
+					down.append(item)
+					item = [0]
+				if sim[i] <= sim[i-1]:
+					trend_mark = "down"
+					# days in the trend
+					item[0] = item[0] +1 
+		#process last group 
+		if trend_mark == "up":
+			up.append(item)
+		if trend_mark == "down":
+			down.append(item)
+		print(up)
+		print(down)
 
 def main():
 	data = csv_reader.csv_array("")
@@ -41,6 +146,11 @@ def main():
 	hp_dp_corr = np.corrcoef(hp, dp)[0]
 	for item in hp_dp_corr:
 		print(item)
+
+	
+	dp_t.print_trend()		
+
+
 
 
 if __name__ == "__main__":
